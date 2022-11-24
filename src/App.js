472f4b545f022136b82cb80965a365de618d45d2
JDaza13 <jdaza1327@gmail.com>
2022-11-24 16:14:34 -0500

    format App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import AddNumber from "./Numbers/AddNumbers";
-import NumbersList from "./Numbers/NumbersList";
-import React, { useState } from "react";
+import AddNumber from './Numbers/AddNumbers';
+import NumbersList from './Numbers/NumbersList';
+import React, { useState } from 'react';
 
 function App() {
-  const [numberList, setNumberList] = useState([]);
-  const addNumberHandler = (uNumber) => {
-    console.log(uNumber)
-    setNumberList((prevNumberList) => {
-      return [
-        ...prevNumberList,
-        { number: uNumber, id: Math.random().toString() },
-      ];
-    });
-  };
+	const [numberList, setNumberList] = useState([]);
+	const addNumberHandler = uNumber => {
+		console.log(uNumber);
+		setNumberList(prevNumberList => {
+			return [
+				...prevNumberList,
+				{
+					number: uNumber,
+					id: Math.random().toString()
+				}
+			];
+		});
+	};
 
-  return (
-    <>
-      <AddNumber onAddNumber={addNumberHandler}></AddNumber>
-      <NumbersList numbers={numberList} />
-    </>
-  );
+	return (
+		<>
+			<AddNumber
+				onAddNumber={addNumberHandler}
+			></AddNumber>
+			<NumbersList numbers={numberList} />
+		</>
+	);
 }
 
 export default App;
